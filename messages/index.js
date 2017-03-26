1a418f28ba5fe93935233da7ef8f3d415dfff78b
JGAJ <James.ga.johnson@gmail.com>
2017-03-25 21:11:50 -0700

    Waterfall

--- messages/index.js
+++ messages/index.js
@@ -36,8 +36,26 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     session.send('Please type again, I didnt understand \'%s\'.', session.message.text);
 });
 
-bot.dialog('/', intents);
-
+//bot.dialog('/', intents);
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                    " you've been programming for " + session.userData.coding + 
+                    " years and use " + session.userData.language + ".");
+    }
+]);
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
