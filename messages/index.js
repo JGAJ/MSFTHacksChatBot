5682eb7cbc3b9d04a6b50c1ffd379c5bd3a5a1ad
JGAJ <James.ga.johnson@gmail.com>
2017-03-26 05:25:41 -0700

    custom prompt

--- messages/index.js
+++ messages/index.js
@@ -8,6 +8,7 @@ http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var genrePrompt = require('./genrePrompt');
+
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
